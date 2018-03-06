Computed velocity and transition state, rearranged files, correct hessian matrix [branch ch112] [branch ch118]

main.py
@@ -7,7 +7,9 @@ from report.length_check import LengthCheck
 from parameter.input_parameters import Parameters
 from pdb.align import Align
 from path.hessian import BuildHessian
-from path.transition_state import ThermoDynamics
+from path.thermo import ThermoDynamics
+import scipy.linalg as sp
+from path.transition_state import Time, TransitionState
 
 parser = argparse.ArgumentParser(description='PATH algorithm')
 subparser = parser.add_subparsers(dest='option')
@@ -43,6 +45,7 @@ args = parser.parse_args()
 if __name__ == '__main__':
     if args.option == 'path':
         parameters = Parameters(args)
+        constant = Constant()
 
         pdb_left = PDBRead(args.start, parameters.c_alpha)
         pdb_right = PDBRead(args.end, parameters.c_alpha)
@@ -61,10 +64,26 @@ if __name__ == '__main__':
 
         thermo = ThermoDynamics()
 
-        work_endpoint = thermo.work(pdb_left.coord, pdb_right.coord)
+        work_endpoint = thermo.work(aligned_left, aligned_right)
 
         energy_left = thermo.energy(hessian_left, work_endpoint)
         energy_right = thermo.energy(hessian_right, work_endpoint)
+
+        eval_left, evec_left = sp.eigh(hessian_left, eigvals=(0, (pdb_left.natoms * constant.dim) - 1))
+        eval_right, evec_right = sp.eigh(hessian_right, eigvals=(0, (pdb_right.natoms * constant.dim) - 1))
+
+        time = Time()
+
+        tbar_left, force_constant_left = time.time_to_transition_state(eval_left)
+        tbar_right, force_constant_right = time.time_to_transition_state(eval_right)
+
+        transition_state = TransitionState(tbar_left, tbar_right, force_constant_left, force_constant_right, eval_left,
+                                           eval_right, evec_left, evec_right, aligned_left, aligned_right,
+                                           pdb_left.natoms)
+
+        energy_left = thermo.energy(hessian_left, transition_state.work_left)
+        energy_right = thermo.energy(hessian_right, transition_state.work_right)
+
 
     elif args.option == 'smooth':
         pdb = PDBRead(args.structure)

path/hessian.py
@@ -105,22 +105,22 @@ class BuildHessian(object):
 
         sidechain_torsion_atoms = []
 
-        for aa in range(len(amino_acid_no)-1):
+        for aa in range(len(amino_acid_no) - 1):
             if amino_acid_present[aa]:
-                if aa == len(amino_acid_no)-2:
+                if aa == len(amino_acid_no) - 2:
                     end = len(amino_acid_no)
                     if amino_acid[aa] != 'GLY' and amino_acid[aa] != 'ALA':
                         tetrad_atoms_list = sidechain.amino_acid(amino_acid[aa], atom_name, start, end)
                         for tetrad in range(len(tetrad_atoms_list)):
                             sidechain_torsion_atoms.append(tetrad_atoms_list[tetrad])
                 else:
-                    if amino_acid_no[aa+1] != amino_acid_no[aa]:
-                        end = aa+1
+                    if amino_acid_no[aa + 1] != amino_acid_no[aa]:
+                        end = aa + 1
                         if amino_acid[aa] != 'GLY' and amino_acid[aa] != 'ALA':
                             tetrad_atoms_list = sidechain.amino_acid(amino_acid[aa], atom_name, start, end)
                             for tetrad in range(len(tetrad_atoms_list)):
                                 sidechain_torsion_atoms.append(tetrad_atoms_list[tetrad])
-                        start = aa+1
+                        start = aa + 1
 
         for tetrad in range(len(sidechain_torsion_atoms)):
             tetrad_coord = []
@@ -234,9 +234,11 @@ class BuildHessian(object):
                     for x in range(constant.dim):
                         for y in range(constant.dim):
                             hessian[(3 * atom_i) + x][(3 * atom_j) + y] = -(
-                                    force_constant * (coord[atom_i][x] - coord[atom_j][y]) * (
+                                    force_constant * (coord[atom_i][x] - coord[atom_j][x]) * (
                                     coord[atom_i][y] - coord[atom_j][y])) / (distance[atom_i][atom_j] * (
                                     np.sqrt(mass.amino_acid_mass(amino_acid[atom_i])) * np.sqrt(
                                 mass.amino_acid_mass(amino_acid[atom_j]))))
+
+                            hessian[(3 * atom_i) + x][(3 * atom_i) + y] += -hessian[(3 * atom_i) + x][(3 * atom_j) + y]
 
         return hessian