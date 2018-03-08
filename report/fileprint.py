9047e5bd1222f71589d4044c1382ab45a1070445
Niranj Chandrasekaran <Srinivas.Chandrasekaran@umassmed.edu>
2018-03-07 22:29:51 -0500

    Array printing functions

--- report/fileprint.py
+++ report/fileprint.py
@@ -3,8 +3,20 @@ class FilePrint(object):
     def __init__(self):
         pass
 
-    def print_array(self):
-        pass
+    def print_array(self, array, filename):
+        fout = open(filename, 'w')
 
-    def print_multi_array(self):
-        pass
+        for row in range(len(array)):
+            fout.write('%f\n' % (array[row]))
+
+        fout.close()
+
+    def print_multi_array(self, array, filename):
+        fout = open(filename, 'w')
+
+        for row in range(len(array)):
+            for col in range(len(array[row])):
+                fout.write('%f\t' % array[row][col])
+            fout.write('\n')
+
+        fout.close()
